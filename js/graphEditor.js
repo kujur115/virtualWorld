a66c626ea6dd598daa7903c06b2c1d015ed659f3
Pratik Kujur <kujurpratik115@gmail.com>
2024-02-13 21:24:35 +0530

    create an editor for the graph

---
+++ js/graphEditor.js
@@ -0,0 +1,62 @@
+class GraphEditor {
+  constructor(canvas, graph) {
+    this.canvas = canvas;
+    this.graph = graph;
+    this.selected = null;
+    this.hovered = null;
+    this.dragging = false;
+    this.mouse = null;
+
+    this.ctx = this.canvas.getContext("2d");
+    this.#addEventListeners();
+  }
+  #addEventListeners() {
+    this.canvas.addEventListener("mousedown", (e) => {
+      if (e.button === 2) {
+        // ? right click
+        if (this.hovered) this.#removePoint(this.hovered);
+        else this.selected = null;
+      } else if (e.button === 0) {
+        // ? left click
+
+        if (this.hovered) {
+          this.#select(this.hovered);
+          this.dragging = true;
+          return;
+        }
+        this.graph.addPoint(this.mouse);
+        this.#select(this.mouse);
+        this.hovered = this.mouse;
+      }
+    });
+    this.canvas.addEventListener("mousemove", (e) => {
+      this.mouse = new Point(e.offsetX, e.offsetY);
+      this.hovered = getNearestPoint(this.mouse, this.graph.points, 10);
+      if (this.dragging) {
+        this.selected.x = this.mouse.x;
+        this.selected.y = this.mouse.y;
+      }
+    });
+    this.canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+    this.canvas.addEventListener("mouseup", () => (this.dragging = false));
+  }
+  #select(point) {
+    if (this.selected)
+      this.graph.tryAddSegment(new Segment(this.selected, point));
+    this.selected = point;
+  }
+  #removePoint(point) {
+    this.graph.removePoint(point);
+    this.hovered = null;
+    if (this.selected == point) this.selected = null;
+  }
+  display() {
+    this.graph.draw(this.ctx);
+    if (this.hovered) this.hovered.draw(this.ctx, { fill: true });
+    if (this.selected) {
+      const intent = this.hovered ? this.hovered : this.mouse;
+      new Segment(this.selected, intent).draw(this.ctx, { dash: [3, 3] });
+      this.selected.draw(this.ctx, { outline: true });
+    }
+  }
+}
---
+++ js/math/utils.js
@@ -0,0 +1,13 @@
+function getNearestPoint(loc, points, threshold = Number.MAX_SAFE_INTEGER) {
+  let minDist = Number.MAX_SAFE_INTEGER;
+  let nearest = null;
+  for (const point of points) {
+    const dist = distance(point, loc);
+    if (dist < minDist && dist < threshold) {
+      minDist = dist;
+      nearest = point;
+    }
+  }
+  return nearest;
+}
+const distance = (p1, p2) => Math.hypot(p1.x - p2.x, p1.y - p2.y);
