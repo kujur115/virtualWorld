1040896d6873590f57ad5eb4ad84f238bbe38dcd
Pratik Kujur <kujurpratik115@gmail.com>
2024-02-14 16:32:13 +0530

    create polygon and envelope around segments

--- js/math/utils.js
+++ js/math/utils.js
@@ -15,3 +15,7 @@ const distance = (p1, p2) => Math.hypot(p1.x - p2.x, p1.y - p2.y);
 const add = (p1, p2) => new Point(p1.x + p2.x, p1.y + p2.y);
 const subtract = (p1, p2) => new Point(p1.x - p2.x, p1.y - p2.y);
 const scale = (p1, scaler) => new Point(p1.x * scaler, p1.y * scaler);
+const translate = (loc, angle, offset) =>
+  new Point(loc.x + offset * Math.cos(angle), loc.y + offset * Math.sin(angle));
+
+const angle = (p) => Math.atan2(p.y, p.x);
